5a5d17f743cc99ee42a922ffba7e9aac74e8ff09
Mayank Raj <mayank.raj3769@gmail.com>
2024-03-27 23:29:30 +0530

    User thread timeline feature done

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -12,7 +12,7 @@ const Page = async ({params} : {params : {id:string} }) =>{
     const user =await currentUser();
     if (!user) return null;
 
-    const userInfo = await  fetchUser(user.id);
+    const userInfo = await fetchUser(user.id);
     if(!userInfo?.onboarded) redirect('/onboarding');
 
     const thread = await fetchThreadById(params.id);
@@ -21,9 +21,8 @@ const Page = async ({params} : {params : {id:string} }) =>{
     <section className="relative">
         <div>
         <ThreadCard
-            key={thread._id}
             id={thread._id}
-            currentUserId={user?.id || ""}
+            currentUserId={user.id}
             parentId={thread.parentId}
             content={thread.text}
             author={thread.author}
@@ -34,7 +33,7 @@ const Page = async ({params} : {params : {id:string} }) =>{
         </div>
         <div className="mt-7 ">
             <Comment
-                threadId={thread.id}
+                threadId={params.id}
                 currentUserImg={user.imageUrl}
                 currentUserId={JSON.stringify(userInfo._id)}
             />
