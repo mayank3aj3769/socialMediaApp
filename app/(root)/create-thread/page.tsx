005626f4420af9e77422d19ffacf82f78872d552
Mayank Raj <mayank.raj3769@gmail.com>
2024-04-04 17:07:44 +0530

    Organization issues

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -4,7 +4,7 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import {redirect} from "next/navigation";
 async function Page(){
-    const user =await currentUser();
+    const user = await currentUser();
     
     if (!user) return null;
     const userInfo=await fetchUser(user.id);
